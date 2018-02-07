3491bccdb01832b238b63a3156c52cdc67910a39
thiko <thimo.koenig@gmail.com>
2018-02-07 20:07:44 +0100

    simple websocket client implementation without wamp (prototype)

--- src/main/java/com/cf/example/PoloniexWSSClientExample.java
+++ src/main/java/com/cf/example/PoloniexWSSClientExample.java
@@ -1,39 +1,32 @@
 package com.cf.example;
 
-import com.cf.client.WSSClient;
-import com.cf.data.handler.poloniex.PoloniexSubscription;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import com.cf.client.PlainWSSClient;
 
 /**
  *
  * @author David
  */
-public class PoloniexWSSClientExample
-{
-    private final static Logger LOG = LogManager.getLogger(PoloniexWSSClientExample.class);
-    private static final String ENDPOINT_URL = "wss://api.poloniex.com";
-    private static final String DEFAULT_REALM = "realm1";
+public class PoloniexWSSClientExample {
+	private final static Logger LOG = LogManager.getLogger(PoloniexWSSClientExample.class);
+	private static final String ENDPOINT_URL = "wss://api2.poloniex.com";
 
-    public static void main(String[] args)
-    {
-        try
-        {
-            new PoloniexWSSClientExample().run();
-        }
-        catch (Exception ex)
-        {
-            LOG.fatal("An exception occurred when running PoloniexWSSClientExample - {}", ex.getMessage());
-            System.exit(-1);
-        }
-    }
+	public static void main(String[] args) {
+		try {
+			new PoloniexWSSClientExample().run();
+		} catch (Exception ex) {
+			LOG.fatal("An exception occurred when running PoloniexWSSClientExample - {}", ex.getMessage());
+			System.exit(-1);
+		}
+	}
 
-    public void run() throws Exception
-    {
-        try (WSSClient poloniexWSSClient = new WSSClient(ENDPOINT_URL, DEFAULT_REALM))
-        {
-            poloniexWSSClient.subscribe(PoloniexSubscription.TICKER);
-            poloniexWSSClient.run(60000);
-        }
-    }
+	public void run() throws Exception {
+
+		try (PlainWSSClient plainWssClient = new PlainWSSClient(ENDPOINT_URL)) {
+			plainWssClient.run(360000);
+			// plainWssClient.subscribe(PoloniexSubscription.TICKER);
+		}
+	}
 }
